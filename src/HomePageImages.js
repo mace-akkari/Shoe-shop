225fbf72e94a6806ec84d5120b59528f7fe60502
Mace Akkari <mace.akkari@relyfe.com>
2022-10-03 10:28:01 +0100

    Styling(Json) add more products

--- src/HomePageImages.js
+++ src/HomePageImages.js
@@ -3,6 +3,7 @@ const HomePageImages = {
   WomensLogo: "images/shop-womens.png",
   MensLogo: "images/shop-mens.png",
   UnisexLogo: "images/shop-unisex.png",
+  RewardsLogo: "images/discount.jpeg",
 };
 
 export default HomePageImages;
--- src/components/Products/SearchMenu.js
+++
@@ -1,7 +0,0 @@
-// import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
-
-// const MensProducts = () => {
-//   return <div>"hi"</div>;
-// };
-
-// export default MensProducts;
